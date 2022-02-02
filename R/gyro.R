4d45b89bae172be7f4cce427d4fa15db6cc10c6b
stla <laurent_step@yahoo.fr>
2022-02-02 03:41:50 +0100

    gyrodemos function

--- R/gyro.R
+++ R/gyro.R
@@ -482,4 +482,40 @@ plotGyrohull3d <- function(
   invisible(NULL)
 }
 
-
+#' @title Examples of the 'gyro' package
+#' @description Some examples of hyperbolic polyhedra realized with the 'gyro'
+#'   package.
+#'
+#' @return No value. The function firstly copies the demo files in a
+#'   temporary directory. If you use RStudio, the function opens these files.
+#'   Otherwise it prints a message giving the instructions to access to these
+#'   files.
+#' @export
+#'
+#' @importFrom rstudioapi isAvailable navigateToFile
+#' @importFrom clipr clipr_available write_clip
+gyrodemos <- function(){
+  folder <- system.file("gyrodemos", package = "gyro")
+  tmpdir <- file.path(tempdir(), "gyrodemos")
+  dir.create(tmpdir)
+  files <- list.files(folder)
+  tmpfiles <- file.path(tmpdir, files)
+  invisible(file.copy(file.path(folder, files), tmpfiles))
+  if(isAvailable(version_needed = "0.99.719")){
+    sapply(tmpfiles, navigateToFile)
+  }else{
+    line <- sprintf(
+      'wd <- setwd("%s")\n',
+      normalizePath(tmpdir, winslash = "/")
+    )
+    message(
+      "Copy the following line to go to the demos folder:\n",
+      line,
+      "Type `setwd(wd)` to come back."
+    )
+    if(clipr_available()){
+      write_clip(line)
+      message("The line has been copied to the clipboard.")
+    }
+  }
+}
